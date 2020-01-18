c43b464e0f821c65fb9efbf553d069cd91bc1ab9
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2020-01-18 12:28:23 +0530

    Initial commit

---
+++ src/GitInfo.macro.js
@@ -0,0 +1,31 @@
+const { execSync } = require('child_process');
+const { createMacro } = require('babel-plugin-macros');
+
+// calling git from the command line should be done just once
+const gitInfo = (() => {
+  const ret = {};
+  try {
+    // TODO try to extract all information from a single `execSync` call
+    ret.branch = execSync('git branch --show-current').toString().trim();
+    ret.commit = {
+      date: execSync('git log --format="%cI" -n 1 HEAD').toString().trim(),
+      message: execSync('git log --format="%B" -n 1 HEAD').toString().trim(),
+      commitHash: execSync('git rev-parse HEAD').toString().trim(),
+      // we could've just taken a substring from the full commit hash but that would ignore
+      // the short commit hash length defined as `core.abbrev` in gitconfig
+      shortCommitHash: execSync('git rev-parse --short HEAD').toString().trim(),
+    };
+  } catch (e) {
+    throw Error(`Unable to parse the git information: ${e}`);
+  }
+  return ret;
+})();
+
+const getGitInformation = ({ references }) => {
+  const sourceString = `(function() { return ${JSON.stringify(gitInfo)}; })`;
+  references.default.forEach(referencePath => {
+    referencePath.replaceWithSourceString(sourceString);
+  });
+};
+
+module.exports = createMacro(getGitInformation);
