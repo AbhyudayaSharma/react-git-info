468a2cb0368ae1184b3379410a7512b3f4a01022
Snaptags <github@ml74.de>
2020-05-02 11:56:36 +0200

    use custom git format to generate a JSON object containing ALL required information

--- src/GitInfo.macro.js
+++ src/GitInfo.macro.js
@@ -1,23 +1,41 @@
 const { execSync } = require('child_process');
 const { createMacro } = require('babel-plugin-macros');
 
-// calling git from the command line should be done just once
+const gitLogToJSON = (() => {
+  const X = "@_!@_@!_@"; // if anyone uses THIS string in her commit message, gitInfo will fail.
+  const commitFormat = `{${X}date${X}: ${X}%cI${X}, ${X}message${X}: ${X}%B${X}, ${X}hash${X}: ${X}%H${X}, ${X}shortHash${X}: ${X}%h${X}}`;
+  const format = `{${X}refs${X}: ${X}%D${X}, ${X}commit${X}: ${commitFormat}}`;
+  const logResult = execSync(`git log --format="${format}" -n 1 HEAD`)
+    .toString()
+    .trim()
+    .replace(/"/g, '\\"') // correctly escape " used in data
+    .replace(/\n/g, "\\n") // at least commit messages may contain newline characters
+    .replace(new RegExp(X, "g"), '"'); // replace custom delimiter by "
+  return JSON.parse(logResult);
+})();
+
+const parseRefs = (refs) => {
+  let branch;
+  const tags = [];
+  refs.split(",").map((item) => {
+    const isBranch = item.match(/HEAD -> (.*)/);
+    const isTag = item.match(/tag: (.*)/);
+
+    if (isTag && isTag.length > 1) {
+      tags.push(isTag[1]);
+    } else {
+      branch = isBranch ? isBranch[1] : branch;
+    }
+  });
+  return [branch, tags];
+};
+
 const gitInfo = (() => {
   const ret = {};
   try {
-    // TODO try to extract all information from a single `execSync` call
-    ret.branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
-    ret.commit = {
-      date: execSync('git log --format="%cI" -n 1 HEAD').toString().trim(),
-      message: execSync('git log --format="%B" -n 1 HEAD').toString().trim(),
-      hash: execSync('git rev-parse HEAD').toString().trim(),
-      // we could've just taken a substring from the full commit hash but that would ignore
-      // the short commit hash length defined as `core.abbrev` in gitconfig
-      shortHash: execSync('git rev-parse --short HEAD').toString().trim(),
-    };
-    // get any tags that ref this commit, filtering out falsy strings (i.e. if the cmd output is empty)
-    ret.tags = execSync(`git tag --list --points-at ${ret.commit.hash}`)
-                .toString().trim().split(/\r?\n/).filter(Boolean);
+    const logResult = gitLogToJSON;
+    [ret.branch, ret.tags] = parseRefs(logResult.refs);
+    ret.commit = logResult.commit;
   } catch (e) {
     throw Error(`Unable to parse the git information: ${e}`);
   }
