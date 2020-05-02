76d399cf85fca5343d29c933704e460ae4ec2124
Snaptags <github@ml74.de>
2020-05-02 07:52:45 +0200

    run prettier to fix indentation

--- macro.d.ts
+++ macro.d.ts
@@ -1,16 +1,16 @@
-declare module 'react-git-info/macro' {
-    export interface GitCommitInformation {
-        readonly date: string;
-        readonly hash: string;
-        readonly message: string;
-        readonly shortHash: string;
-    }
+declare module "react-git-info/macro" {
+  export interface GitCommitInformation {
+    readonly date: string;
+    readonly hash: string;
+    readonly message: string;
+    readonly shortHash: string;
+  }
 
-    export interface GitInformation {
-        readonly tags: string[];
-        readonly branch: string;
-        readonly commit: GitCommitInformation;
-    }
+  export interface GitInformation {
+    readonly tags: string[];
+    readonly branch: string;
+    readonly commit: GitCommitInformation;
+  }
 
-    export default function GitInfo(): GitInformation;
+  export default function GitInfo(): GitInformation;
 }
