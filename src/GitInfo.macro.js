042e20944207e822da7d5057e334f310faa0dec3
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2020-05-04 12:44:57 +0530

    Fix warnings
    * Use `'` instead of `"`.
    * Throw error if branch was not parsed.
    * Rename `parseGitLog` to `parsedGitLog`.

--- src/GitInfo.macro.js
+++ src/GitInfo.macro.js
@@ -1,24 +1,25 @@
 const { createMacro } = require('babel-plugin-macros');
 const { execSync } = require('child_process');
 
-const parseGitLog = (() => {
-  let message = '';
-  let refs = '';
+const parsedGitLog = (() => {
+  let message;
+  let refs;
   const commit = {};
   // only the commit message can have multiple lines. Make sure to always add at the end:
+  // The format is specified in https://git-scm.com/docs/git-log#_pretty_formats
   const logResult = execSync('git log --format="%D%n%h%n%H%n%cI%n%B" -n 1 HEAD')
     .toString()
     .trim()
     .split(/\r?\n/);
   [refs, commit.shortHash, commit.hash, commit.date, ...message] = logResult;
-  commit.message = message.join("\n");
+  commit.message = message.join('\n');
   return {refs, commit};
 })();
 
 const parseRefs = (refs) => {
-  let branch;
+  let branch = undefined;
   const tags = [];
-  refs.split(", ").map((item) => {
+  refs.split(', ').map((item) => {
     const isBranch = item.match(/HEAD -> (.*)/);
     const isTag = item.match(/tag: (.*)/);
 
@@ -28,13 +29,18 @@ const parseRefs = (refs) => {
       branch = isBranch ? isBranch[1] : branch;
     }
   });
+
+  if (!branch) {
+    throw Error('Unable to get parse branch from the log result.');
+  }
+
   return [branch, tags];
 };
 
 const gitInfo = (() => {
   const ret = {};
   try {
-    const logResult = parseGitLog;
+    const logResult = parsedGitLog;
     [ret.branch, ret.tags] = parseRefs(logResult.refs);
     ret.commit = logResult.commit;
   } catch (e) {
