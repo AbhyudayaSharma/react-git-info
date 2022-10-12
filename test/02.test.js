d58dc7799be7f11dd0551a18cb681d7fb3002a71
Jakob Andrén <jakob.andren@impulseradar.se>
2022-10-12 14:33:41 +0200

    Update tests for latest create-react-app

--- test/02.test.js
+++ test/02.test.js
@@ -4,7 +4,7 @@ const gitInfo = GitInfo();
 
 describe('Git information', () => {
   test('gets correct branch', () => {
-    expect(gitInfo.branch).toBe('master');
+    expect(gitInfo.branch).toBe('main');
   });
 
   test('gets correct tags', () => {
